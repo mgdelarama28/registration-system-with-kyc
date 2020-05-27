836956db8440354db9bf8d2774c67f9c6f19a8e3
Michael Geofrey Dela Rama <mgdelarama.28@gmail.com>
2020-05-27 15:45:37 +0800

    More conflict

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,10 +1,6 @@
 <footer class="main-footer">
 	<div class="float-right d-none d-sm-inline">
-<<<<<<< HEAD
-		Anything you want
-=======
 		v1.0
->>>>>>> a3d5f9a965df91611a581cb0824bc407fb3bae26
 	</div>
 	
 	<strong>Copyright &copy; 2020 <a href="https://adminlte.io">MasterAdmin</a>.</strong> All rights reserved.
